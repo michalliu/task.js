6d8b6b5fd9942dd12ff7b98a925f1de68c760ede
michalliu <michalliu@foxmail.com>
2013-01-30 23:52:32 +0800

    initial upload

---
+++ src/task.js
@@ -0,0 +1,26 @@
+function task() {
+	if (!(this instanceof task)) {
+		return new task();
+	}
+	this.todos=[];
+	this.sleepTotal = 0;
+}
+task.prototype = {
+	run: function (fn) {
+		this.todos.push(fn);
+		return this;
+	},
+	sleep: function (time) {
+		this.sleepTotal += time;
+		var worker = new Worker('/src/timer.js');
+		var todos = this.todos;
+		this.todos = [];
+		worker.onmessage = function(event) {
+			todos.forEach(function (todo) {
+				todo();
+			});
+		};
+		worker.postMessage(this.sleepTotal);
+		return this;
+	}
+};
