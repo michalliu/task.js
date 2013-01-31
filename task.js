638a44a59282e476723214af3c27604fb4861ee3
michalliu <michalliu@foxmail.com>
2013-02-01 01:02:13 +0800

    增加执行时间统计与简易服务器端

--- task.js
+++ task.js
@@ -1,42 +1,46 @@
-function task() {
-	if(!(this instanceof task)) {
-		return new task();
+(function () {
+	'use strict';
+	function Task() {
+		if (!(this instanceof Task)) {
+			return new Task();
+		}
+		this.timer = [];
+		this.todo = {};
+		this.timeout = 0;
 	}
-	this.timer=[];
-	this.todo={};
-	this.timeout=0;
-}
-task.prototype = {
-	run: function (fn) {
-		var lastTimer;
-		if (this.timer.length <= 0) {
-			fn();
-		} else {
-			lastTimer = this.timer[this.timer.length - 1];
-			if (!this.todo[lastTimer]) {
-				this.todo[lastTimer] = [];
+	Task.prototype = {
+		run: function (fn) {
+			var lastTimer;
+			if (this.timer.length <= 0) {
+				fn();
+			} else {
+				lastTimer = this.timer[this.timer.length - 1];
+				if (!this.todo[lastTimer]) {
+					this.todo[lastTimer] = [];
+				}
+				this.todo[lastTimer].push(fn);
 			}
-			this.todo[lastTimer].push(fn);
+			return this;
+		},
+		sleep: function (time) {
+			if (typeof time === 'funtion') {
+				time = time();
+			}
+			var that = this;
+			that.timeout += time;
+			var timer = setTimeout(function () {
+				var callbacks = that.todo[timer];
+				callbacks.forEach(function (cb) {
+					cb();
+				});
+			}, that.timeout);
+			this.timer.push(timer);
+			return this;
+		},
+		done: function (fn) {
+			this.sleep(0);
+			this.run(fn);
 		}
-		return this;
-	},
-	sleep: function (time) {
-		if (typeof time === 'funtion') {
-			time = time();
-		}
-		var that = this;
-		that.timeout += time;
-		var timer = setTimeout(function () {
-			var callbacks = that.todo[timer];
-			var start = Date.now();
-			var delay;
-			callbacks.forEach(function (cb) {
-				cb();
-			});
-			delay = Date.now() - start;
-			that.timeout += delay;
-		}, that.timeout);
-		this.timer.push(timer);
-		return this;
-	}
-};
+	};
+	window.task = Task;
+}());
