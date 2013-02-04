a4d8605eb104e987198becfbae9de1478ceab0fd
michalliu <michalliu@foxmail.com>
2013-02-04 19:55:21 +0800

    增加assert和返回值传递的演示

--- task.js
+++ task.js
@@ -1,18 +1,28 @@
 (function () {
 	'use strict';
+	function assertionError(message) {
+		this.message = message;
+		this.toString = function () {
+			return "Assertion failed: " + this.message;
+		};
+	}
+
 	function Task() {
 		if (!(this instanceof Task)) {
 			return new Task();
 		}
+		var undef;
 		this.timer = [];
 		this.todo = {};
 		this.timeout = 0;
+		this.lastResult = undef;
 	}
+
 	Task.prototype = {
 		run: function (fn) {
 			var lastTimer;
 			if (this.timer.length <= 0) {
-				fn();
+				this.lastResult = fn(this.lastResult);
 			} else {
 				lastTimer = this.timer[this.timer.length - 1];
 				if (!this.todo[lastTimer]) {
@@ -30,11 +40,43 @@
 			that.timeout += time;
 			var timer = setTimeout(function () {
 				var callbacks = that.todo[timer];
-				callbacks.forEach(function (cb) {
-					cb();
-				});
+				if(callbacks && callbacks.forEach) {
+					callbacks.forEach(function (cb) {
+						that.lastResult = cb(that.lastResult);
+					});
+				}
 			}, that.timeout);
 			this.timer.push(timer);
+			return this;
+		},
+		assert: function (assertFn) {
+			var that = this;
+			var timers;
+			if (typeof assertFn === "function"){
+				this.sleep(0).run(function (d) {
+					var ret = assertFn(d);
+					var statement = assertFn.toString();
+					if (!ret) {
+						if (task.onAssertionError) {
+							try{
+								assertionError.prototype = new Error();
+								throw new assertionError(statement);
+							} catch (ex) {
+								task.onAssertionError(ex);
+							}
+						} else {
+							if (window.console && console.assert) {
+								console.assert(ret,statement);
+							} else {
+								throw new assertionError(statement);
+							}
+						}
+						timers = that.timer.forEach(function (timer) {
+							clearTimeout(timer);
+						});
+					}
+				}).sleep(0);
+			}
 			return this;
 		}
 	};
