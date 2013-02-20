079cb02fce8a5371658f39678d5348fc694aa7fd
michalliu <michalliu@foxmail.com>
2013-02-20 16:40:35 +0800

    replace typeof with isFunction method

--- task.js
+++ task.js
@@ -1,10 +1,16 @@
+/*jshint browser:true*/
+/*global console*/
 (function () {
-	'use strict';
-	function assertionError(message) {
+	"use strict";
+	function AssertionFail(message) {
 		this.message = message;
 		this.toString = function () {
 			return "Assertion failed: " + this.message;
 		};
+	}
+
+	function isFunction(fn) {
+		return typeof fn === "function";
 	}
 
 	function Task() {
@@ -12,29 +18,37 @@
 			return new Task();
 		}
 		var undef;
-		this.timer = [];
-		this.todo = {};
-		this.timeout = 0;
-		this.lastResult = undef;
+		this.timer = []; // timer list
+		this.todo = {}; // todo list, the key is timer
+		this.timeout = 0; // current timeout value
+		this.lastResult = undef; // last value returned from function
 	}
 
 	Task.prototype = {
 		run: function (fn) {
 			var lastTimer;
-			if (this.timer.length <= 0) {
-				this.lastResult = fn(this.lastResult);
-			} else {
-				lastTimer = this.timer[this.timer.length - 1];
-				if (!this.todo[lastTimer]) {
-					this.todo[lastTimer] = [];
+			if (isFunction(fn)) {
+				if (this.timer.length <= 0) {
+					this.lastResult = fn(this.lastResult);
+				} else {
+					lastTimer = this.timer[this.timer.length - 1];
+					if (!this.todo[lastTimer]) {
+						this.todo[lastTimer] = [];
+					}
+					this.todo[lastTimer].push(fn);
 				}
-				this.todo[lastTimer].push(fn);
 			}
 			return this;
 		},
 		sleep: function (time) {
-			if (typeof time === 'funtion') {
-				time = time();
+			if (isFunction(time)) {
+				time = time(this.lastResult);
+				if (typeof time !== "number") { // parse time to number
+					time = parseInt(time, 10);
+				}
+				if (isNaN(time)) { // we expect a number
+					time = 0;
+				}
 			}
 			var that = this;
 			that.timeout += time;
@@ -56,21 +70,21 @@
 				var fn = fns[0];
 				var lastVal;
 				var val;
-				if (typeof fn === "function") {
+				if (isFunction(fn)) {
 					lastVal = fn(d);
 				} else {
 					lastVal = fn;
 				}
 				for (;i<l;i++) {
 					fn = fns[i];
-					if (typeof fn === "function") {
+					if (isFunction(fn)) {
 						val = fn(d);
 					} else {
 						val = fn;
 					}
 					if (val !== lastVal) {
 						fns = null;
-						return [false, fn + " should " + (typeof fn === "function" ? "return ": "be ") + lastVal];
+						return [false, fn + " should " + (isFunction(fn) ? "return ": "be ") + lastVal];
 					}
 				}
 				fns = null;
@@ -84,21 +98,21 @@
 				var fn = fns[0];
 				var lastVal;
 				var val;
-				if (typeof fn === "function") {
+				if (isFunction(fn)) {
 					lastVal = fn(d);
 				} else {
 					lastVal = fn;
 				}
 				for (;i<l;i++) {
 					fn = fns[i];
-					if (typeof fn === "function") {
+					if (isFunction(fn)) {
 						val = fn(d);
 					} else {
 						val = fn;
 					}
 					if (val === lastVal) {
 						fns = null;
-						return [false, fn + " should not " + (typeof fn === "function" ? "return ": "be ") + lastVal];
+						return [false, fn + " should not " + (isFunction(fn) ? "return ": "be ") + lastVal];
 					}
 				}
 				fns = null;
@@ -107,38 +121,50 @@
 		},
 		assertFalse: function (fn) {
 			return this.assert(function (d) {
-				return [fn(d) === false, fn + " should " + (typeof fn === "function" ? "return ": "be ") + "false"];
+				var ret;
+				if (isFunction(fn)) {
+					ret = fn(d);
+				} else {
+					ret = fn;
+				}
+				return [ret === false, fn + " should " + (isFunction(fn) ? "return ": "be ") + "false"];
 			});
 		},
 		assertTrue: function (fn) {
 			return this.assert(function (d) {
-				return [fn(d) === true, fn + " should " + (typeof fn === "function" ? "return ": "be ") + "true"];
+				var ret;
+				if (isFunction(fn)) {
+					ret = fn(d);
+				} else {
+					ret = fn;
+				}
+				return [ret === true, fn + " should " + (isFunction(fn) ? "return ": "be ") + "true"];
 			});
 		},
 		assert: function (assertFn) {
 			var that = this;
-			if (typeof assertFn === "function"){
+			if (isFunction(assertFn)){
 				this.sleep(0).run(function (d) {
 					var result = assertFn(d);
-					var ret = result[0];
-					var statement = result[1];
-					if (!ret) {
-						if (task.onAssertionError) {
+					var ret = result[0]; // assert success or not
+					var statement = result[1]; // tips message
+					if (!ret) { // assertion failed
+						if (Task.onAssertionFail) { // has assertion error handler
 							try{
-								assertionError.prototype = new Error();
-								throw new assertionError(statement);
+								AssertionFail.prototype = new Error(); // generate error stack
+								throw new AssertionFail(statement);
 							} catch (ex) {
-								task.onAssertionError(ex);
+								Task.onAssertionFail(ex); // let execption handler handle it
 							}
 						} else {
 							if (window.console && console.assert) {
-								console.assert(ret,statement);
+								console.assert(ret,statement);// let console handles it
 							} else {
-								throw new assertionError(statement);
+								throw new AssertionFail(statement); // or throw it
 							}
 						}
 						that.timer.forEach(function (timer) {
-							clearTimeout(timer);
+							clearTimeout(timer); // stop the timer associate with functions not yet executed
 						});
 					}
 				}).sleep(0);
