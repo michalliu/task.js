faae37f56455ae8fdb43a3a47fcc2cd1458baf57
michalliu <michalliu@foxmail.com>
2013-02-20 21:23:15 +0800

    add more comment and more method

--- task.js
+++ task.js
@@ -3,9 +3,10 @@
 ;(function () {
 	"use strict";
 
-	var todoFlag = "todo",
-		timerFlag = "timer";
+	var todoFlag = "todo", // flag to mark the nextsibling is a todo(function)
+		timerFlag = "timer";// flag to mark the nextsibling is a timer(function)
 
+	// assertion fail error object
 	function AssertionFail(message) {
 		this.message = message;
 		this.toString = function () {
@@ -13,32 +14,43 @@
 		};
 	}
 
+	// check if function
 	function isFunction(fn) {
 		return typeof fn === "function";
 	}
 
+	// the task constructor
 	function Task() {
 		if (!(this instanceof Task)) {
 			return new Task();
 		}
-		this.queue = []; // event queue
-		this.timer = []; // store timers
-		this.timeout = 0; // timeout recorder
-		this.todo = {}; // todo list
+		this.queue = []; // the original event queue
+		this.timer = []; // store all the timers
+		this.timeout = 0; // timeout total
+		this.protective = true; // protect the queue
+		this.todo = {}; // a map record the timer and it's callbacks
 	}
 
+	// prototype
 	Task.prototype = {
-		run: function (fn) { // put to run queue
+		// put something to run queue
+		run: function (fn) {
 			this.queue.push(todoFlag);
 			this.queue.push(fn);
 			return this;
 		},
-		sleep: function (time) { // put to sleep queue
+		// put something to sleep queque, time or time generator
+		sleep: function (time) {
 			this.queue.push(timerFlag);
 			this.queue.push(time);
 			return this;
 		},
-		_run: function (fn) { // actually run
+		// the actual run method
+		// if no timer set then set a timer(0ms delay), then bind the rest
+		// "run" function to that timer
+		// if timer already set, just bind the function to that
+		// timer
+		_run: function (fn) {
 			var lastTimer;
 			if (isFunction(fn)) {
 				if (this.timer.length <= 0) {
@@ -53,6 +65,12 @@
 				}
 			}
 		},
+		// the actual sleep method
+		// 1. if time is a function rather than number, execute it, use the return value
+		// as the delaying time
+		// 2. the time always added the last time
+		// 3. generate a timer, push to timer queque, the callback function will execute
+		// any function which binded to that timer
 		_sleep: function (time) { // actually sleep
 			if (isFunction(time)) {
 				time = time(this.lastResult);
@@ -64,20 +82,27 @@
 				}
 			}
 			var that = this;
+			var timer;
+			// determin timeout total
 			that.timeout += time;
-			var timer = setTimeout(function () {
+			// generate timer
+			timer = setTimeout(function () {
+				// retrieve "run" functions assoicate with timer
 				var callbacks = that.todo[timer];
+				// must be an array
 				if(callbacks && callbacks.forEach) {
 					callbacks.forEach(function (cb) {
+						// record the last result returned by the "run" function
 						that.lastResult = cb(that.lastResult);
 					});
 				}
 			}, that.timeout);
+			// store timer
 			this.timer.push(timer);
 		},
 		assertEquals: function () {
 			var fns = arguments;
-			return this.assert(function (d) {
+			return this._assert(function (d) {
 				var i=1,l=fns.length;
 				var fn = fns[0];
 				var lastVal;
@@ -100,12 +125,12 @@
 					}
 				}
 				fns = null;
-				return [true, ""];
+				return [true, ""]; // assertion result, message
 			});
 		},
 		assertNotEquals:function () {
 			var fns = arguments;
-			return this.assert(function (d) {
+			return this._assert(function (d) {
 				var i=1,l=fns.length;
 				var fn = fns[0];
 				var lastVal;
@@ -132,7 +157,7 @@
 			});
 		},
 		assertFalse: function (fn) {
-			return this.assert(function (d) {
+			return this._assert(function (d) {
 				var ret;
 				if (isFunction(fn)) {
 					ret = fn(d);
@@ -143,7 +168,7 @@
 			});
 		},
 		assertTrue: function (fn) {
-			return this.assert(function (d) {
+			return this._assert(function (d) {
 				var ret;
 				if (isFunction(fn)) {
 					ret = fn(d);
@@ -153,13 +178,12 @@
 				return [ret === true, fn + " should " + (isFunction(fn) ? "return ": "be ") + "true"];
 			});
 		},
-		assert: function (assertFn) {
+		_assert: function (assertFn) {
 			var that = this;
 			if (isFunction(assertFn)){
-				this.sleep(0);
-				this.run(function (d) {
-					var result = assertFn(d);
-					var ret = result[0]; // assert success or not
+				this.sleep(0).run(function (d) {
+					var result = assertFn(d); // run assertion
+					var ret = result[0]; // determin assert result
 					var statement = result[1]; // tips message
 					if (!ret) { // assertion failed
 						if (Task.onAssertionFail) { // has assertion error handler
@@ -173,15 +197,23 @@
 							if (window.console && console.assert) {
 								console.assert(ret,statement);// let console handles it
 							} else {
+								if (that.protective && that._onInterrupt) { // trigger interrupt event
+									that._onInterrupt();
+								}
 								throw new AssertionFail(statement); // or throw it
 							}
 						}
-						that.timer.forEach(function (timer) {
-							clearTimeout(timer); // stop the timer associate with functions not yet executed
-						});
+						// stop the timer associate with functions not yet executed
+						if (that.protective) {
+							if (that._onInterrupt) { // trigger interrupt event
+								that._onInterrupt();
+							}
+							that.timer.forEach(function (timer) {
+								clearTimeout(timer);
+							});
+						}
 					}
-				});
-				this.sleep(0);
+				}).sleep(0); // make sure assertion is running on a stand alone context
 			}
 			return this;
 		},
@@ -211,34 +243,57 @@
 			do{
 				for (var i=0,step=0;i<q.length;i+=2) {
 					one = q[i];
-					if (one === todoFlag) {
+					if (one === todoFlag) { // next one is todo function
 						fn = q[i+1];
 						this._run(fn);
 						// insert a function to emit a progress event
+						// but not in the original running queque
+						// it's much more like a ghost
 						if (onProgress && isFunction(onProgress)) {
 							this._run(progressEmitter(step++, currentRepeat));
 						}
-					} else if (one === timerFlag) {
+					} else if (one === timerFlag) { // next one is a timer(generator)
 						fn = q[i+1];
 						this._sleep(fn);
 					}
 				}
-				this._sleep(0); // start over
+				this._sleep(0); // start over, run queque again, no delay
 			} while(++currentRepeat <= maxRepeat);
-			if(this._onDone) {
+			if(this._onDone) { // trigger done event
 				this._run(this._onDone);
 			}
 		},
+		// set progress callback
 		progress: function (fn) {
 			if (isFunction(fn)) {
 				this._onProgress = fn;
 			}
 			return this;
 		},
+		interrupt: function (fn) {
+			if (isFunction(fn)) {
+				this._onInterrupt = fn;
+			}
+			return this;
+		},
+		// set done callback
 		done: function (fn) {
 			if (isFunction(fn)) {
 				this._onDone = fn;
 			}
+			return this;
+		},
+		// if protect the queue, if first assertionError happend
+		// the queue will stoped to execute, this is the default action
+		protect: function () {
+			this.protective = true;
+			return this;
+		},
+		// if unprotect the queue, if first assertionError happend
+		// the queue will continue to execute, which may cause unexpected
+		// result
+		unprotect: function () {
+			this.protective = false;
 			return this;
 		}
 	};
